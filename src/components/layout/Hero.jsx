7234c909c1ffcf243c15d87170a53c80f3b36aed
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-01-13 13:14:52 +0530

    landing page done

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -3,129 +3,95 @@ import logo from "../../assets/image/image5.png";
 import image1 from "../../assets/image/image6.png";
 import magicbutton from "../../assets/image/magicbutton1.png";
 import svg from "../../assets/svg/wave-orange.svg";
+import svg1 from "../../assets/svg/MixedColor.svg";
+import image7 from "../../assets/image/image7.png";
 
 function Hero() {
   return (
     <div>
-      <section className="full p-[1rem]">
-        <div className="flex items-center justify-between gap-1 ">
-          <div
-            className="
-    absolute
-    w-32 h-32
-    -translate-y-16 -translate-x-7
+      <section
+        className=" flex  flex-col lg:flex-row justify-between items-center   lg:gap-1  p-2 
 
-    sm:w-60 sm:h-60
-    sm:-translate-y-10 sm:-translate-x-2
+"
+      >
+        <div className=" w-[100%] lg:w-[25%] absolute flex flex-col justify-start gap-10 sm:gap-2 md:gap-10 top-10 right-5  md:flex lg:static lg:min-h-screen lg:items-end">
+          {/* left side imgage */}
 
-    md:w-[16.5rem] md:h-[16.5rem]
-    md:-translate-y-17 md:-translate-x-9
-
-    lg:static
-    lg:w-[27rem] lg:h-[27rem]
-
-    lg:flex lg:flex-col
-    lg:justify-between
-  
- 
-  "
-          >
+          <div className="w-[9.5rem]  sm:w-[13rem]  md:w-[14rem] lg:w-[100%]">
             <img
               src={image1}
-              alt="Decorative illustration"
-              className="w-full h-full object-cover
-           
-              
-              "
-            />
-            <img
-              src={svg}
               alt=""
-              className="   lg:translate-y-12
-            lg:-rotate-15
-            
-            "
+              className="w-[100%] object-cover h-[100%]"
             />
-            <div>
-              <h1
-                className="
-              hidden 
-              lg:block
-              w-[25rem]
-              lg:absolute
-              lg:translate-y-25
-              lg:-rotate-7
-              lg:flex
-              items-center
-              "
-              >
-                <span
-                  className="
-                     text-center
-                   lg:border-orange-400
-                     lg:border-2 
-                     lg:rounded-4xl 
-                     lg:w-fit
-                     lg:px-3 
-                     lg:py-2 
-                     lg:text-3xl 
-                     lg:rounded-4xl
-                     lg:text-center 
-                     lg:-rotate-5"
-                >
-                  create Magic
-                </span>
+          </div>
+          <div className="-rotate-10 sm:rotate-3 md:rotate-5 lg:-rotate-20 ">
+            <img src={svg} alt="" className=" lg:w-[20rem]" />
+          </div>
+          <div className="w-[11.5rem] flex -rotate-12 mx-7 items-center   lg:w-[16rem] ">
+            <h1 className="border-2 border-amber-300 rounded-4xl w-full text-center px-2 py-2 lg:text-xl lg:border-3">
+              create buttom
+            </h1>
+            <img
+              src={magicbutton}
+              alt=""
+              className="w-[4rem] h-[4rem] object-cover lg:w-[6rem] lg:h-[6rem]"
+            />
+          </div>
+        </div>
+
+        {/* middile text */}
+
+        <div className=" w-[100%]  lg:w-[50%] lg:min-h-screen">
+          {" "}
+          <div className="text-center font-medium">
+            <p className="text-gray-500 text-[0.9rem] lg:text-xl">
+              Igniting the Spark of Inspiration
+            </p>
+            <h1 className="text-[2.3rem] md:text-[4.5rem] lg:text-[5.5rem] font-medium leading-[1.05] text-center relative">
+              <span className="block">Unleash Your</span>
+
+              <span className="flex items-center justify-center my-2">
+                <span>Brand</span>
+
+                {/* Center Logo */}
                 <img
-                  src={magicbutton}
-                  alt=""
-                  className="w-[6rem] h-[6rem] object-cover"
+                  src={logo}
+                  alt="logo"
+                  className="w-15 h-15 md:w-12 md:h-12 lg:w-25 lg:h-30 object-contain lg:pt-5"
                 />
-              </h1>
-            </div>
-          </div>
 
-          <div>
-            <div className="text-center font-medium">
-              <p className="text-gray-500 text-[0.9rem] lg:text-xl">
-                Igniting the Spark of Inspiration
-              </p>
-              <h1 className="text-[2.7rem] md:text-[4.5rem] lg:text-[5.5rem] font-medium leading-[1.05] text-center relative">
-                <span className="block">Unleash Your</span>
+                <span>with Our</span>
+              </span>
 
-                <span className="flex items-center justify-center my-2">
-                  <span>Brand</span>
-
-                  {/* Center Logo */}
-                  <img
-                    src={logo}
-                    alt="logo"
-                    className="w-15 h-15 md:w-12 md:h-12 lg:w-25 lg:h-30 object-contain lg:pt-5"
-                  />
-
-                  <span>with Our</span>
+              <span className="block mt-2">
+                <span className="inline-block bg-yellow-200 text-purple-700 px-4 leading-none rounded-xl rotate-[3deg] mr-3 pb-4">
+                  Mágico
                 </span>
-
-                <span className="block mt-2">
-                  <span className="inline-block bg-yellow-200 text-purple-700 px-4 leading-none rounded-xl rotate-[3deg] mr-3 pb-4">
-                    Mágico
-                  </span>
-                  Design
-                </span>
-              </h1>
+                Design
+              </span>
+            </h1>
+            <div className="hidden lg:block w-[100%]  lg:flex justify-center items-center pt-20">
+              <img src={svg1} alt="" className=" lg:w-[25rem] rotate-6" />
             </div>
           </div>
+        </div>
 
-          <div className="bg-amber-600 flex flex-col justify-between h-full">
-            <div>
-              <h1>Via della createvitta</h1>
-              <h1>20212 milano</h1>
-            </div>
-            <div>
-              <h1>Via della createvitta</h1>
-              <h1>20212 milano</h1>
-            </div>
-            <div></div>
+        {/* last div */}
+        <div className="absolute w-[70%] right-0 top-42 flex flex-col items-end justify-end  p-[1rem] overflow-hidden lg:justify-between lg:static lg:w-[25%] lg:items-start  lg:min-h-screen lg:justify-start  lg:gap-30   ">
+          <div className="hidden lg:block lg:flex lg:flex-col lg:gap-1 items end justify-end">
+            <h1 className="bg-purple-700 rounded-4xl text-white px-7 py-2 ">Via della createvitta</h1>
+            <h1 className="bg-purple-700 rounded-4xl text-white text-end px-5 py-1 w-fit ">20212 milano</h1>
           </div>
+          <div className="w-[9rem] flex  justify-end lg:w-[25rem] lg:justify-start lg:-px-8 ">
+            <img
+              src={image7}
+              alt=""
+              className="w-[9rem] h-auto  -mx-9 rotate-2 lg:w-[25rem]   "
+            />
+          </div>
+                <div className="lg:hidden">
+              <img src={svg1} alt="" className=" lg:w-[20rem] rotate-6" />
+            </div>
         </div>
       </section>
     </div>
